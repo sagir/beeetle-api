49759f22e7b0ebcce5d314fbd9166001f060e122
Sagir Hossain <sagir.hossain.524@gmail.com>
2021-08-27 02:14:38 +0600

    auth route and controller added

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,7 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class AuthController {
+  async login({}: HttpContextContract) {}
+
+  async logout({}: HttpContextContract) {}
+}
