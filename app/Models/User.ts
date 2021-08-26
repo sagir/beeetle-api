7a67673c7e95376fc76fdea73f110aee903f12b4
Sagir Hossain <sagir.hossain.524@gmail.com>
2021-08-27 01:11:11 +0600

    deactivatedAt property added to User Model

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -15,6 +15,9 @@ export default class User extends BaseModel {
   @column()
   public rememberMeToken?: string
 
+  @column.dateTime()
+  public deactivatedAt: DateTime
+
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
