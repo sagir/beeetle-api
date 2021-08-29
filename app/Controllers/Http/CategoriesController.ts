0fd62dee2abd35ef62be0f55543ea33207b1a3c5
Sagir Hossain <sagir.hossain.524@gmail.com>
2021-08-30 01:31:07 +0600

    categories index api done

--- app/Controllers/Http/CategoriesController.ts
+++ app/Controllers/Http/CategoriesController.ts
@@ -1,7 +1,28 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { ModelPaginatorContract } from '@ioc:Adonis/Lucid/Orm'
+import Category from 'App/Models/Category'
 
 export default class CategoriesController {
-  public async index({}: HttpContextContract) {}
+  public async index({
+    bouncer,
+    request,
+  }: HttpContextContract): Promise<ModelPaginatorContract<Category>> {
+    await bouncer.with('CategoryPolicy').authorize('view')
+
+    const page = request.input('page', 1)
+    const perPage = request.input('perPage', 10)
+    const active = request.input('activeItems')
+    const orderBy = request.input('orderBy', 'name')
+    const orderDirection = request.input('orderDirection', 'asc')
+
+    const query = Category.query()
+
+    if (active !== undefined) {
+      query.withScopes((q) => (active ? q.active() : q.inactive()))
+    }
+
+    return await query.orderBy(orderBy, orderDirection).paginate(page, perPage)
+  }
 
   public async store({}: HttpContextContract) {}
 
