14ce574dc9873d6d47b04b652872d0e9ff9230e1
Sagir Hossain <sagir.hossain.524@gmail.com>
2021-08-29 14:03:11 +0600

    user seeder added

---
+++ database/seeders/User.ts
@@ -0,0 +1,25 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import User from 'App/Models/User'
+import Role from 'App/Models/Role'
+
+export default class UserSeeder extends BaseSeeder {
+  public async run () {
+    const superAdmin = new User()
+    superAdmin.name = 'Sagir Hossain'
+    superAdmin.email = 'sagir@admin.com'
+    superAdmin.password = '123456'
+    await superAdmin.save()
+
+    const manager = new User()
+    manager.name = 'John Doe'
+    manager.email = 'john@admin.com'
+    superAdmin.password = '123456'
+    await superAdmin.save()
+
+    const superAdminRole = await Role.findByOrFail('slug', 'super-admin')
+    const managerAdminRole = await Role.findByOrFail('slug', 'manager')
+
+    await superAdmin.related('roles').attach([superAdminRole.id])
+    await manager.related('roles').attach([managerAdminRole.id])
+  }
+}
