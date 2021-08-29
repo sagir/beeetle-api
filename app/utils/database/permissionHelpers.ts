13aae5d3b33330c197705a8bbfb6144b7cd6372c
Sagir Hossain <sagir.hossain.524@gmail.com>
2021-08-29 15:59:08 +0600

    permissionHelpers eslint formatting fixed

--- app/utils/database/permissionHelpers.ts
+++ app/utils/database/permissionHelpers.ts
@@ -1,19 +1,22 @@
-import Permission from "App/Models/Permission";
-import User from "App/Models/User";
+import Permission from 'App/Models/Permission'
+import User from 'App/Models/User'
 
 export const hasPermission = async (
   user: User,
   model: string,
   action: string
 ): Promise<boolean> => {
-  const permission = await Permission.query().where([
-    ['model', model],
-    ['action', action]
-  ]).whereHas('roles', rolesQuery => {
-    rolesQuery.whereHas('users', usersQuery => {
-      usersQuery.where('id', user.id)
+  const permission = await Permission.query()
+    .where([
+      ['model', model],
+      ['action', action],
+    ])
+    .whereHas('roles', (rolesQuery) => {
+      rolesQuery.whereHas('users', (usersQuery) => {
+        usersQuery.where('id', user.id)
+      })
     })
-  }).first()
+    .first()
 
   return !!permission
 }
@@ -26,15 +29,24 @@ export const hasPermissions = async (
     return true
   }
 
-  const permissionModels = await Permission.query().where(query => {
-    query
-      .whereIn('model', permissions.map(({ model }) => model))
-      .andWhereIn('action', permissions.map(({ action }) => action))
-  }).whereHas('roles', rolesQuery => {
-    rolesQuery.whereHas('users', usersQuery => {
-      usersQuery.where('id', user.id)
+  const permissionModels = await Permission.query()
+    .where((query) => {
+      query
+        .whereIn(
+          'model',
+          permissions.map(({ model }) => model)
+        )
+        .andWhereIn(
+          'action',
+          permissions.map(({ action }) => action)
+        )
     })
-  }).exec()
+    .whereHas('roles', (rolesQuery) => {
+      rolesQuery.whereHas('users', (usersQuery) => {
+        usersQuery.where('id', user.id)
+      })
+    })
+    .exec()
 
   return permissionModels.length === permissions.length
 }
