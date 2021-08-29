4864e732a406185dddf897587fa3e2a95f463866
Sagir Hossain <sagir.hossain.524@gmail.com>
2021-08-30 02:40:07 +0600

    supplier model and migration added

---
+++ app/Models/Supplier.ts
@@ -0,0 +1,31 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Supplier extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public email: string
+
+  @column({ serializeAs: null })
+  public password: string
+
+  @column()
+  public phone: string
+
+  @column()
+  public address?: string
+
+  @column.dateTime()
+  public deactivatedAt?: DateTime
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
