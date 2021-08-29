c9c6ed300ad80251bfd4ea5c12839fba83f13bcc
Sagir Hossain <sagir.hossain.524@gmail.com>
2021-08-29 19:11:28 +0600

    users controller created

---
+++ app/Controllers/Http/UsersController.ts
@@ -0,0 +1,21 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class UsersController {
+  public async index({}: HttpContextContract) {}
+
+  public async store({}: HttpContextContract) {}
+
+  public async show({}: HttpContextContract) {}
+
+  public async update({}: HttpContextContract) {}
+
+  public async destroy({}: HttpContextContract) {}
+
+  public async activate({}: HttpContextContract) {}
+
+  public async deactivate({}: HttpContextContract) {}
+
+  public async roles({}: HttpContextContract) {}
+
+  public async routes({}: HttpContextContract) {}
+}
