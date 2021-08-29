2688dab5b3b4680ccc80cfb1277c7421355ad9fd
Sagir Hossain <sagir.hossain.524@gmail.com>
2021-08-29 16:04:45 +0600

    scope queries fixed

--- app/utils/database/scopes.ts
+++ app/utils/database/scopes.ts
@@ -2,9 +2,13 @@ import { LucidModel, LucidRow, ModelQueryBuilderContract } from '@ioc:Adonis/Luc
 import { DateTime } from 'luxon'
 
 export function active(query: ModelQueryBuilderContract<LucidModel, LucidRow>) {
-  query.whereNull('deactivated_at').orWhere('deactivated_at', '>', DateTime.now().toSQL())
+  query.where((query) => {
+    query.whereNull('deactivated_at').orWhere('deactivated_at', '>', DateTime.now().toSQL())
+  })
 }
 
 export function inactive(query: ModelQueryBuilderContract<LucidModel, LucidRow>) {
-  query.whereNotNull('deactivated_at').andWhere('deactivated_at', '<=', DateTime.now().toSQL())
+  query.where((query) => {
+    query.whereNotNull('deactivated_at').andWhere('deactivated_at', '<=', DateTime.now().toSQL())
+  })
 }
