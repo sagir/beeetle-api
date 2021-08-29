eeb1cd64d6bb3508f26f1983930956ec90af1e6e
Sagir Hossain <sagir.hossain.524@gmail.com>
2021-08-29 22:54:23 +0600

    stores index api done

--- app/Controllers/Http/StoresController.ts
+++ app/Controllers/Http/StoresController.ts
@@ -1,7 +1,28 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { ModelPaginatorContract } from '@ioc:Adonis/Lucid/Orm'
+import Store from 'App/Models/Store'
 
 export default class StoresController {
-  public async index({}: HttpContextContract) {}
+  public async index({
+    bouncer,
+    request,
+  }: HttpContextContract): Promise<ModelPaginatorContract<Store>> {
+    await bouncer.with('StorePolicy').authorize('view')
+
+    const page = request.input('page', 1)
+    const perPage = request.input('perPage', 10)
+    const active = request.input('activeItems')
+    const orderBy = request.input('orderBy', 'name')
+    const orderDirection = request.input('orderDirection', 'name')
+
+    const query = Store.query()
+
+    if (active !== undefined) {
+      query.withScopes((q) => (active ? q.active() : q.inactive()))
+    }
+
+    return await query.orderBy(orderBy, orderDirection).paginate(page, perPage)
+  }
 
   public async store({}: HttpContextContract) {}
 
