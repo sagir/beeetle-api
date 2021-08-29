2a59d2c20f57a029820ba2d0ba508d59a426ff30
Sagir Hossain <sagir.hossain.524@gmail.com>
2021-08-30 03:08:40 +0600

    supplier index api done

--- app/Controllers/Http/SuppliersController.ts
+++ app/Controllers/Http/SuppliersController.ts
@@ -1,7 +1,28 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { ModelPaginatorContract } from '@ioc:Adonis/Lucid/Orm'
+import Supplier from 'App/Models/Supplier'
 
 export default class SuppliersController {
-  public async index({}: HttpContextContract) {}
+  public async index({
+    bouncer,
+    request,
+  }: HttpContextContract): Promise<ModelPaginatorContract<Supplier>> {
+    await bouncer.with('SupplierPolicy').authorize('view')
+
+    const page = request.input('page', 1)
+    const perPage = request.input('perPage', 10)
+    const active = request.input('activeItems')
+    const orderBy = request.input('orderBy', 'name')
+    const orderDirection = request.input('orderDirection', 'asc')
+
+    const query = Supplier.query().orderBy(orderBy, orderDirection)
+
+    if (active !== undefined) {
+      query.withScopes((q) => (active ? q.active() : q.inactive()))
+    }
+
+    return await query.paginate(page, perPage)
+  }
 
   public async store({}: HttpContextContract) {}
 
