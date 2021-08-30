6df3b0f6fc7b7b592bc36c74dfc3e4886bd58f13
Sagir Hossain <sagir.hossain.524@gmail.com>
2021-08-30 15:40:29 +0600

    SpecificationPolicy fixed

--- app/Policies/SpecificationPolicy.ts
+++ app/Policies/SpecificationPolicy.ts
@@ -6,26 +6,26 @@ export default class SpecificationPolicy extends BasePolicy {
   private model: string = 'specification'
 
   public async view(user: User): Promise<boolean> {
-    return hasPermission(user, this.model, 'read')
+    return await hasPermission(user, this.model, 'read')
   }
 
   public async create(user: User): Promise<boolean> {
-    return hasPermission(user, this.model, 'create')
+    return await hasPermission(user, this.model, 'create')
   }
 
   public async update(user: User): Promise<boolean> {
-    return hasPermission(user, this.model, 'update')
+    return await hasPermission(user, this.model, 'update')
   }
 
   public async delete(user: User): Promise<boolean> {
-    return hasPermission(user, this.model, 'delete')
+    return await hasPermission(user, this.model, 'delete')
   }
 
   public async activate(user: User): Promise<boolean> {
-    return hasPermission(user, this.model, 'activate')
+    return await hasPermission(user, this.model, 'activate')
   }
 
   public async deactivate(user: User): Promise<boolean> {
-    return hasPermission(user, this.model, 'deactivate')
+    return await hasPermission(user, this.model, 'deactivate')
   }
 }
