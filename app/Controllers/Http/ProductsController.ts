ebd4b269d754843e2de8914cee91d27776fbe22d
Sagir Hossain <sagir.hossain.524@gmail.com>
2021-08-30 21:26:30 +0600

    product store api done

--- app/Controllers/Http/ProductsController.ts
+++ app/Controllers/Http/ProductsController.ts
@@ -1,6 +1,8 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
 import { ModelPaginatorContract } from '@ioc:Adonis/Lucid/Orm'
 import Product from 'App/Models/Product'
+import ProductValidator from 'App/Validators/ProductValidator'
 
 export default class ProductsController {
   public async index({
@@ -31,7 +33,37 @@ export default class ProductsController {
     return await query.paginate(page, perPage)
   }
 
-  public async store({}: HttpContextContract) {}
+  public async store({ bouncer, request, response }: HttpContextContract): Promise<void> {
+    await bouncer.with('ProductPolicy').authorize('create')
+    await request.validate(ProductValidator)
+    const product = new Product()
+    const trx = await Database.transaction()
+
+    product.name = request.input('name')
+    product.slug = request.input('slug')
+    product.code = request.input('code')
+    product.description = request.input('description')
+
+    try {
+      await product.useTransaction(trx).save()
+      await product.related('categories').attach(request.input('categories'), trx)
+      await product.related('specifications').attach(
+        request.input('specifications').map((item) => {
+          let obj = {}
+          obj[item.id] = { value: item.value }
+        }),
+        trx
+      )
+      await trx.commit()
+    } catch (error) {
+      trx.rollback()
+      return response.internalServerError({
+        message: 'Something went wrong. Please try again.',
+      })
+    }
+
+    return response.created(product)
+  }
 
   public async show({}: HttpContextContract) {}
 
