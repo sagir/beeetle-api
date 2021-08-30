aafd6400cd5afe16985478f709dd081225259496
Sagir Hossain <sagir.hossain.524@gmail.com>
2021-08-30 14:49:28 +0600

    specification routes and controller methods added

---
+++ app/Controllers/Http/SpecificationsController.ts
@@ -0,0 +1,17 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class SpecificationsController {
+  public async index({}: HttpContextContract) {}
+
+  public async store({}: HttpContextContract) {}
+
+  public async show({}: HttpContextContract) {}
+
+  public async update({}: HttpContextContract) {}
+
+  public async destroy({}: HttpContextContract) {}
+
+  public async activate({}: HttpContextContract) {}
+
+  public async deactivate({}: HttpContextContract) {}
+}
---
+++ start/routes/specifications.ts
@@ -0,0 +1,14 @@
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.group(() => {
+  Route.get('', 'SpecificationsController.index').as('index')
+  Route.post('', 'SpecificationsController.store').as('store')
+  Route.get(':id', 'SpecificationsController.show').as('show')
+  Route.put(':id', 'SpecificationsController.update').as('update')
+  Route.delete(':id', 'SpecificationsController.destroy').as('destroy')
+  Route.patch(':id/activate', 'SpecificationsController.activate').as('activate')
+  Route.patch(':id/deactivate', 'SpecificationsController.deactivate').as('deactivate')
+})
+  .prefix('api/v1/specifications')
+  .middleware('auth')
+  .as('api.v1.specifications')
