a055dfbdafaf60de9e8009bd5d4f0c67b40fa38b
Sagir Hossain <sagir.hossain.524@gmail.com>
2021-08-30 15:54:33 +0600

    products index api done

--- app/Controllers/Http/ProductsController.ts
+++ app/Controllers/Http/ProductsController.ts
@@ -1,7 +1,35 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { ModelPaginatorContract } from '@ioc:Adonis/Lucid/Orm'
+import Product from 'App/Models/Product'
 
 export default class ProductsController {
-  public async index({}: HttpContextContract) {}
+  public async index({
+    bouncer,
+    request,
+  }: HttpContextContract): Promise<ModelPaginatorContract<Product>> {
+    await bouncer.with('ProductPolicy').authorize('view')
+
+    const page = request.input('page', 1)
+    const perPage = request.input('perPage', 10)
+    const search = request.input('query')
+    const active = request.input('activeItems')
+    const orderBy = request.input('orderBy', 'name')
+    const orderDirection = request.input('orderDirection', 'asc')
+
+    const query = Product.query().orderBy(orderBy, orderDirection)
+
+    if (active !== undefined) {
+      query.withScopes((q) => (active ? q.active() : q.inactive()))
+    }
+
+    if (search) {
+      query.where((q) => {
+        q.where('name', 'like', `%${search}%`).orWhere('description', 'like', `%${search}%`)
+      })
+    }
+
+    return await query.paginate(page, perPage)
+  }
 
   public async store({}: HttpContextContract) {}
 
