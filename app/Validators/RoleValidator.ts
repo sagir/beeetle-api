cfdb0cd5fa50aacb6d2094d1ca867c60656cd1f5
Sagir Hossain <sagir.hossain.524@gmail.com>
2021-08-30 18:50:41 +0600

    roles validation eslint fix

--- app/Validators/RoleValidator.ts
+++ app/Validators/RoleValidator.ts
@@ -2,63 +2,55 @@ import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class RoleValidator {
-  constructor (protected ctx: HttpContextContract, private roleId: number = 0) {
-  }
+  constructor(protected ctx: HttpContextContract, private roleId: number = 0) {}
 
-	/*
-	 * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
-	 *
-	 * For example:
-	 * 1. The username must be of data type string. But then also, it should
-	 *    not contain special characters or numbers.
-	 *    ```
-	 *     schema.string({}, [ rules.alpha() ])
-	 *    ```
-	 *
-	 * 2. The email must be of data type string, formatted as a valid
-	 *    email. But also, not used by any other user.
-	 *    ```
-	 *     schema.string({}, [
-	 *       rules.email(),
-	 *       rules.unique({ table: 'users', column: 'email' }),
-	 *     ])
-	 *    ```
-	 */
+  /*
+   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
+   *
+   * For example:
+   * 1. The username must be of data type string. But then also, it should
+   *    not contain special characters or numbers.
+   *    ```
+   *     schema.string({}, [ rules.alpha() ])
+   *    ```
+   *
+   * 2. The email must be of data type string, formatted as a valid
+   *    email. But also, not used by any other user.
+   *    ```
+   *     schema.string({}, [
+   *       rules.email(),
+   *       rules.unique({ table: 'users', column: 'email' }),
+   *     ])
+   *    ```
+   */
   public schema = schema.create({
-    name: schema.string({ trim: true }, [
-      rules.minLength(3),
-      rules.maxLength(100)
-    ]),
+    name: schema.string({ trim: true }, [rules.minLength(3), rules.maxLength(100)]),
     slug: schema.string({ trim: true }, [
       rules.minLength(3),
       rules.maxLength(100),
       rules.unique({
         table: 'roles',
         column: 'slug',
-        whereNot: { id: this.roleId }
-      })
+        whereNot: { id: this.roleId },
+      }),
     ]),
     description: schema.string.optional({ trim: true }, [
       rules.minLength(10),
-      rules.maxLength(1000)
+      rules.maxLength(1000),
     ]),
-    permissions: schema.array([
-      rules.minLength(1)
-    ]).members(schema.number([
-      rules.unsigned()
-    ]))
+    permissions: schema.array([rules.minLength(1)]).members(schema.number([rules.unsigned()])),
   })
 
-	/**
-	 * Custom messages for validation failures. You can make use of dot notation `(.)`
-	 * for targeting nested fields and array expressions `(*)` for targeting all
-	 * children of an array. For example:
-	 *
-	 * {
-	 *   'profile.username.required': 'Username is required',
-	 *   'scores.*.number': 'Define scores as valid numbers'
-	 * }
-	 *
-	 */
+  /**
+   * Custom messages for validation failures. You can make use of dot notation `(.)`
+   * for targeting nested fields and array expressions `(*)` for targeting all
+   * children of an array. For example:
+   *
+   * {
+   *   'profile.username.required': 'Username is required',
+   *   'scores.*.number': 'Define scores as valid numbers'
+   * }
+   *
+   */
   public messages = {}
 }
