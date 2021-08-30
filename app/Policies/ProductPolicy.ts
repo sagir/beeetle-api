99f3f0e8fb5acc7e3a149e2bbc48060109d758f5
Sagir Hossain <sagir.hossain.524@gmail.com>
2021-08-30 15:42:11 +0600

    ProductPolicy added

---
+++ app/Policies/ProductPolicy.ts
@@ -0,0 +1,31 @@
+import { BasePolicy } from '@ioc:Adonis/Addons/Bouncer'
+import User from 'App/Models/User'
+import { hasPermission } from 'App/utils/database/permissionHelpers'
+
+export default class ProductPolicy extends BasePolicy {
+  private model: string = 'product'
+
+  public async view(user: User): Promise<boolean> {
+    return await hasPermission(user, this.model, 'read')
+  }
+
+  public async create(user: User): Promise<boolean> {
+    return await hasPermission(user, this.model, 'create')
+  }
+
+  public async update(user: User): Promise<boolean> {
+    return await hasPermission(user, this.model, 'update')
+  }
+
+  public async delete(user: User): Promise<boolean> {
+    return await hasPermission(user, this.model, 'delete')
+  }
+
+  public async activate(user: User): Promise<boolean> {
+    return await hasPermission(user, this.model, 'activate')
+  }
+
+  public async deactivate(user: User): Promise<boolean> {
+    return await hasPermission(user, this.model, 'deactivate')
+  }
+}
